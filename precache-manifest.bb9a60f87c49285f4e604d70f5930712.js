86a73fffd50ef38d9cd8a85e352d83ac79f8b964
Brahma100 <brahma.jaiswal100@gmail.com>
2021-11-17 10:52:47 +0530

    Updates

---
+++ precache-manifest.bb9a60f87c49285f4e604d70f5930712.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151544a0e6fec09009d0758e893d88e1",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "f47f5d2494e878bec477",
+    "url": "/Covid-Tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "651c8e89c28bc3708040",
+    "url": "/Covid-Tracker/static/css/main.57051a04.chunk.css"
+  },
+  {
+    "revision": "f47f5d2494e878bec477",
+    "url": "/Covid-Tracker/static/js/2.418c64d0.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/Covid-Tracker/static/js/2.418c64d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "651c8e89c28bc3708040",
+    "url": "/Covid-Tracker/static/js/main.645dba76.chunk.js"
+  },
+  {
+    "revision": "ae45445b70030a69d951",
+    "url": "/Covid-Tracker/static/js/runtime-main.d4cdb983.js"
+  }
+]);
